4a8f6bb8c1893fb1a8f10128d8b874354c779952
Alan Smith <commit@alanwsmith.com>
2023-12-06 22:41:05 -0500

    Day 6 - Part 2

--- day_06_part_02/benches/benchmark.rs
+++ day_06_part_02/benches/benchmark.rs
@@ -2,9 +2,14 @@ use day_06_part_02::Race;
 use day_06_part_02::Solver;
 
 fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn solve_test() {
     let mut s = Solver::new();
     s.races = vec![
         Race { time: 45977295, record: 305106211101695 },
     ];
-    dbg!(s.solve());
+    s.solve();
 }
---
+++ day_06_part_02/src/lib.rs
@@ -0,0 +1,71 @@
+pub struct Race {
+    pub time: usize,
+    pub record: usize,
+}
+
+impl Race {
+    pub fn possible_solutions(&self) -> usize {
+        let mut new_records = 0;
+        for ms in 0..=self.time {
+            let speed = ms;
+            let tics = self.time - ms;
+            let distance = speed * tics;
+            if distance > self.record {
+                new_records += 1;
+            }
+        }
+        new_records
+    }
+}
+
+pub struct Solver {
+    pub input: Option<String>,
+    pub races: Vec<Race>,
+}
+
+impl Solver {
+    pub fn new() -> Solver {
+        Solver {
+            input: None,
+            races: vec![],
+        }
+    }
+
+    pub fn solve(&self) -> usize {
+        self.races
+            .iter()
+            .fold(1, |acc, race| acc * race.possible_solutions())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn integration_1() {
+        let mut s = Solver::new();
+        s.races = vec![
+            Race { time: 7, record: 9 },
+            Race {
+                time: 15,
+                record: 40,
+            },
+            Race {
+                time: 30,
+                record: 200,
+            },
+        ];
+        let left = 288;
+        let right = s.solve();
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn race_possibilities() {
+        let r = Race { time: 7, record: 9 };
+        let left = 4;
+        let right = r.possible_solutions();
+        assert_eq!(left, right);
+    }
+}
